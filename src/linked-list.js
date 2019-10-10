1f9f6c956943ac7aefd15f9bc8d62c7e397e27e9
Iharson <mashniakou@gmail.com>
2019-10-10 19:12:40 +0300

    feat: add partial solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,40 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        let newNode = new Node(data);
 
-    head() {}
+        if (this._head == null) {
+            this._head = newNode;
+            this._tail = newNode;
+        } else {
+            this._tail.next = newNode;
+            newNode.prev = this._tail;
+            this._tail = newNode;
+        }
+        this.length++;
+    }
 
-    tail() {}
+
+    head() {
+        if (this._head != null) {
+            return this._head.data;
+        }
+        return null;
+    }
+
+    tail() {
+        if (this._tail != null) {
+            return this._tail.data;
+        }
+        return null;
+    }
 
     at(index) {}
 
